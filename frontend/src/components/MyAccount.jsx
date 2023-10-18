eb803db933e710b60206c1ce75d658818f8cd806
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-10-18 11:59:54 +0200

    add logout button, with redirect on Login page

--- frontend/src/components/MyAccount.jsx
+++ frontend/src/components/MyAccount.jsx
@@ -5,10 +5,15 @@ import Footer from "./Footer";
 import "./MyAccount.css";
 
 function MyAccount() {
-  const { token } = useAuth();
+  const { token, setToken } = useAuth();
   return (
     <div className="MyAccountBox">
       <Header />
+      <div>
+        <button type="button" onClick={() => setToken(null)}>
+          <NavLink to="/Login">Deconnection</NavLink>
+        </button>
+      </div>
       {token && (
         <>
           <div className="connexion-box">
